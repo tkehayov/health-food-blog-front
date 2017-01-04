07e22b3a31cf20402c13f51818b0564a6012b596
kehayov <tihomirkehayov@deltasource.eu>
2017-01-04 20:49:47 +0200

    add created reciept date

--- src/app/admin/reciept/reciept.controller.js
+++ src/app/admin/reciept/reciept.controller.js
@@ -10,8 +10,10 @@
     var vm = this;
 
     vm.reciept = {};
+    console.log(moment().format());
 
     vm.add = function() {
+      vm.reciept.createdDate = moment().format('YYYY-MM-DD[T]HH:mm:ss');
       $http.post('http://localhost:8080/reciepts', vm.reciept).then(function(){
         console.log("success");
       }, function(){
