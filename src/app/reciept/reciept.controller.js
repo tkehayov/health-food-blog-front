823c80a9ac0dfb48210d6bc2d1ef10b0d8147f64
kehayov <tihomirkehayov@deltasource.eu>
2017-01-06 14:32:41 +0200

    list reciept and display title, inngredients, cooking time  and image

--- src/app/reciept/reciept.controller.js
+++ src/app/reciept/reciept.controller.js
@@ -6,9 +6,21 @@
     .controller('RecieptController', RecieptController);
 
   /** @ngInject */
-  function RecieptController($http) {
+  function RecieptController($http, $stateParams) {
     var vm = this;
+    var recieptId = $stateParams.id;
+    vm.reciept = {};
 
+    $http({
+      method: 'GET',
+      url: 'http://localhost:8080/' + recieptId + '/reciepts'
+    }).then(function successCallback(reciept) {
+      console.log(reciept.data);
+      vm.reciept = reciept.data;
+    }, function errorCallback() {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
 
   }
 })();
