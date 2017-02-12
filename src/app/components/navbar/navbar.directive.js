f73d45bbcfd7a67989dafcb0bacb600f94b2729f
kehayov <tihomirkehayov@deltasource.eu>
2017-02-12 18:54:03 +0200

    add categories

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -6,11 +6,15 @@
     .directive('acmeNavbar', acmeNavbar);
 
   /** @ngInject */
-  function acmeNavbar() {
+  function acmeNavbar(CATEGORIES) {
+
     var directive = {
       restrict: 'E',
       templateUrl: 'app/components/navbar/navbar.html',
       controllerAs: 'vm',
+      link: function(scope) {
+        scope.categories = CATEGORIES;
+      },
       bindToController: true
     };
 
