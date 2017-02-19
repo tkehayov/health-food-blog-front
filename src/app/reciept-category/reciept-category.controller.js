b685647cf8d10e393558c1db892c9a5b1325065c
kehayov <tihomirkehayov@deltasource.eu>
2017-02-19 19:23:07 +0200

    reciepts page

--- src/app/reciept-category/reciept-category.controller.js
+++ src/app/reciept-category/reciept-category.controller.js
@@ -8,11 +8,19 @@
 	/** @ngInject */
 	function RecieptCategoryController($http, $stateParams, CATEGORIES, BACKEND_URL) {
 		var vm = this;
-		var recieptId = $stateParams.id;
-		vm.reciept = {};
-		vm.imageUrl = BACKEND_URL + "/images";
-		vm.categories = CATEGORIES;
-		
-		console.log("RecieptCategoryController");
+		vm.reciepts = [];
+		vm.category = $stateParams.category;
+
+		$http({
+			method: 'GET',
+			url: BACKEND_URL + "/reciepts/" + vm.category + "/categories/"
+		}).then(function successCallback(reciepts) {
+			vm.reciepts = reciepts.data;
+
+			console.log(reciepts);
+		}, function errorCallback() {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+		});
 	}
 })();
