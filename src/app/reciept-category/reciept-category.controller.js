29da5d49206aa36dfc27b2ffb0370a83c49fc88f
kehayov <tihomirkehayov@deltasource.eu>
2017-02-26 10:49:32 +0200

    add category breadcrumbs

--- src/app/reciept-category/reciept-category.controller.js
+++ src/app/reciept-category/reciept-category.controller.js
@@ -5,7 +5,6 @@
 		.module('cakeryFront')
 		.controller('RecieptCategoryController', RecieptCategoryController);
 
-	/** @ngInject */
 	function RecieptCategoryController($http, $stateParams, CATEGORIES, BACKEND_URL) {
 		var vm = this;
 		vm.reciepts = [];
@@ -19,8 +18,6 @@
 			url: BACKEND_URL + "/reciepts/" + vm.category + "/categories/" + vm.subCategory
 		}).then(function successCallback(reciepts) {
 			vm.reciepts = reciepts.data;
-
-			console.log(reciepts);
 		}, function errorCallback() {
 			// called asynchronously if an error occurs
 			// or server returns response with an error status.
